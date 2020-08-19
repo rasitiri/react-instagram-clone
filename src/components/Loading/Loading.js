f68351d8af5221f3ed660f9abcb31b2babadb679
rasitiri <rasitiri3@gmail.com>
2020-08-19 17:43:51 +0300

    Refactor loading component.

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,8 +1,15 @@
 import React from 'react'
-import loadingSVG from '../../assets/images/loading1.svg'
+import loadingSVG from '../../assets/images/loading.svg'
 
 const Loading = () => (
-  <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'80vh'}}>
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '90vh',
+    }}
+  >
     <img style={{ backgroundColor: 'transparent' }} src={loadingSVG} alt="" />
   </div>
 )
