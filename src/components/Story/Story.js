7816be04af9e113efc35fa38bfe8b852f196cd0a
rasitiri <rasitiri3@gmail.com>
2020-08-20 14:56:58 +0300

    Created story field.

---
+++ src/components/Story/Story.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import classes from './Story.module.scss'
+
+const Story = () => (
+  <div className={classes.storyContainer}>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    <div className={classes.storyItem}>
+      <img
+        src="https://scontent-ort2-1.cdninstagram.com/v/t51.2885-19/72465946_535169153695040_2506708321726103552_n.jpg?_nc_ht=scontent-ort2-1.cdninstagram.com&_nc_ohc=FtEESPzmxIUAX8VxzLr&oh=2279b548ce5d12f54cbcec181dd81714&oe=5F6716FD"
+        alt=""
+      />
+      <span>_rl9</span>
+    </div>
+    
+  </div>
+)
+
+export default Story
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import firebase from '../../firebase'
-import { useSelector } from 'react-redux'
+import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import Menu from '../../components/Navbar/Menu'
+import classes from './Home.module.scss'
+import Story from '../../components/Story/Story'
 
 const Home = () => {
-  const [isAuth, setIsAuth] = useState(false)
-  const auth = useSelector(state => state.authReducer.isLoggedIn)
-
-  useEffect(() => {
-    setIsAuth(firebase.isAuthenticated())
-  }, [])
-
-
   return (
     <div>
       <Navbar></Navbar>
       <Menu></Menu>
-      <h1>Home Page</h1>
-      <h2>Status: {isAuth ? 'Giriş yapılmış ' : 'Giriş yapılMAMIŞ'}</h2>
-      <h3>User: {firebase.getCurrentUserInfo()}</h3>
-      <h2>Status : {auth === true ? 'doğru' : 'yanlış'}</h2>
+      <div className={classes.content}>
+        <div className={classes.fieldOne}>
+          <Story></Story>
+        </div>
+        <div className={classes.fieldTwo}>Field 2</div>
+      </div>
     </div>
   )
 }
